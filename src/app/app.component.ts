8eafa492aa832c2745d924601b8682daf4fb02e0
Jaime Carballo Diaz Infante <jaime.carballo.diaz@gmail.com>
2020-11-30 17:26:35 -0600

    Actualizar a core-js y corregir error de npm install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,18 @@ import { ServiciosGeneralesService } from './servicios/servicios-generales.servi
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 
 // Fechas Recarga
 // U1-20R    	2020-08-15		2020-09-15
 // U2-17R    	2020-11-17		2021-01-03
 
-export class AppComponent  {
-  readonly Constants= Constants;
+export class AppComponent {
+  readonly Constants = Constants;
 
-  constructor(public servicioMensajes:MensajesService,public serviciosGenerales :ServiciosGeneralesService ){
+  constructor(public servicioMensajes: MensajesService, public serviciosGenerales: ServiciosGeneralesService) {
+
 
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
